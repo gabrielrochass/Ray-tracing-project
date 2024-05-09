0ecfba855f82054af2e57a15b159ae41e61b810d
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-05-09 20:11:07 -0300

    fix: organizing folders

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include "ponto.cpp"
+#include "vector.cpp"
+
+using namespace std;
+
+int main(){
+    Ponto p1 = Ponto(1, 2, 3); // ponto ok
+    return 0;
+}
