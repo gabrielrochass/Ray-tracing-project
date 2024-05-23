445893d118f85ff59b7695ecb60c2bd1cd5432c4
João Teles <127795554+JoaoTeles87@users.noreply.github.com>
2024-05-23 10:55:50 -0300

    camera estruturada como classe

--- camera.h
+++ camera.h
@@ -1,11 +1,14 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
 #include <iostream>
 #include <cmath>
-#include "ponto.h"
+#include "Ponto.h"
 #include "vector.h"
 
 using namespace std;
 
-// Estrutura Camera
+// Classe Camera
 // câmera móvel -> pode ser posicionada em qualquer lugar do mundo e apontada para diferentes localizações
 // atributos: posicaoDaCamera, mira, vUp, vOrtonomais (u, v, w), distanciaParaTela, larguraDaTela, alturadaTela
 // mira -> Ponto para onde a câmera está apontada
@@ -16,18 +19,21 @@ using namespace std;
 // v -> vetor perpendicular a u e w -> define o vetor para cima da câmera
 
 
-struct Camera {
+class Camera {
     // define atributos
-    vetor<double> posicaoDaCamera;
-    vetor<double> mira;
-    vetor<double> vUp;
-    vetor<double> w, u, v;
-    
-    // construtor
-    Camera(const vetor<double>& posicaoDaCamera_, const vetor<double>& mira_, const vetor<double>& vUp_) : posicaoDaCamera(posicaoDaCamera_), mira(mira_), vUp(vUp_) {
-        w = vetorUni(subtracao(mira, posicaoDaCamera));
-        u = vetorUni(produtoVetorial(vUp, w));
-        v = produtoVetorial(w, u);
-    }
+    public:
+        vetor<double> posicaoDaCamera;
+        vetor<double> mira;
+        vetor<double> vUp;
+        vetor<double> w, u, v;
+        
+        // construtor
+        Camera(const vetor<double>& posicaoDaCamera_, const vetor<double>& mira_, const vetor<double>& vUp_) : posicaoDaCamera(posicaoDaCamera_), mira(mira_), vUp(vUp_) {
+            w = vetorUni(subtracao(mira, posicaoDaCamera));
+            u = vetorUni(produtoVetorial(vUp, w));
+            v = produtoVetorial(w, u);
+        }
 
 };
+
+#endif // CAMERA_H
