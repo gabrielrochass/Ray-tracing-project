e9e6c58d76549883d9cd669f16ca10f99f1083a2
João Teles <127795554+JoaoTeles87@users.noreply.github.com>
2024-05-23 10:57:10 -0300

    main2 simplificada

--- main2.cpp
+++ main2.cpp
@@ -3,30 +3,17 @@
 #include <vector>
 #include "ponto.h"
 #include "vector.h"
-#include "camera.cpp"
+#include "camera.h"
 #include "sphere_list.h"
 #include "sphere.h"
+#include "plano.h"
 #include <limits>
 
 using namespace std;
 
 const double infinity = std::numeric_limits<double>::infinity();
 
-// calcula a interseção de um raio com uma esfera
-double hit(const vetor<double>& center, double radius, const raio<double>& raio) {
-    double a = produtoEscalar(raio.direcao, raio.direcao);
-    vetor<double> oc = subtracao(center, raio.origem);
-    double metadeb = produtoEscalar(raio.direcao, oc);
-    double b = 2.0 * produtoEscalar(raio.direcao, oc);
-    double c = produtoEscalar(oc, oc) - radius * radius;
-    double discriminant = metadeb * metadeb -  a * c;
 
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-metadeb - sqrt(discriminant)) /  a;
-    }
-}
 
 // define a cor do fundo
 vetor<double> backgroundColor(const vetor<double>& dir) {
@@ -36,25 +23,17 @@ vetor<double> backgroundColor(const vetor<double>& dir) {
                          (1 - t) * 1.0 + t * 1.0);
 }
 
-// define se um raio intersecta ou não um plano -> retorna true se intersecta, false caso contrário
-bool hitPlano(const vetor<double>& pontoNoPlano, const vetor<double>& normal, const raio<double>& raioDeInterseccao) {
-    double denominador = produtoEscalar(normal, raioDeInterseccao.direcao); // usado pra determinar se o raio é paralelo ao plano
-    // Verifica se o denominador é zero para evitar divisão por zero
-    if (fabs(denominador) < 1e-6) { // 0
-        return false; // Raio é paralelo ao plano -> não intersecta
-    }
-    // Calcula a distância para o ponto de interseção ao longo do raio
-    double t = -produtoEscalar(subtracao(raioDeInterseccao.origem, pontoNoPlano), normal) / denominador;
-    return t >= 0; // Se t for maior ou igual a zero, o raio intersecta o plano
-} //**************
+
 
 // define a cor do pixel de acordo com a interseção do raio com a esfera
 vetor<double> raioColor(const raio<double>& raio, const sphere_list& mundo) {
     hit_record rec;
+    plano plan({0.0, -1.25, -1.0}, {0.0, 1.0, 0.0});
     if(mundo.hit(raio, 0, infinity, rec)){
         return multiplicacaoPorEscalar(vetor<double>(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1),0.5);
     }
-    else if(hitPlano({0.0, -1.25, -1.0}, {0.0, 1.0, 0.0}, raio)) {
+   
+    else if(plan.hitPlano(plan, raio)) {
         return vetor<double>(1.0, 1.0, 1.0);
     }
     vetor<double> direcao_uni = vetorUni(raio.direcao);
@@ -83,10 +62,8 @@ int main() {
     // define a viewport
     const vetor<double> larguraDaViewport(32.0 / 9.0, 0.0, 0.0);
     const vetor<double> alturaDaViewport(0.0, 2.0, 0.0);
-
-    // define o canto esquerdo da tela -> ponto de origem do raio
     vetor<double> cantoEsquerdoTela = subtracao(subtracao(subtracao(camera.posicaoDaCamera, mult(0.5, larguraDaViewport)), mult(0.5, alturaDaViewport)), mira);
-    
+    //vetor cantoEsquerdoTela = origem - horizontal/2 - vertical/2 - mira
     // define a esfera
     vetor<double> sphereCenter{0.0, 0.0, -1.0};
     double sphereRadius = 0.5;
