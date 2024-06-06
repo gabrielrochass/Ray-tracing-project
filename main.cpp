62aab910250714910e3ad474f5c6fc4b4ce93e90
João Teles <jant@cin.ufpe.br>
2024-06-06 11:56:58 -0300

    feat: update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,103 +3,102 @@
 #include <vector>
 #include "ponto.h"
 #include "vector.h"
-#include "camera.cpp"
+#include "camera.h"
 #include "sphere_list.h"
 #include "sphere.h"
+#include "plano.h" 
+#include <limits>
+#include <memory>
 #include "malha_triangulos.h"
 #include "triangulo.h"
-#include "hit_record.h"
-#include <limits>
-
+#include "raio.h"
+#include "matriz4x4.h"
 using namespace std;
 
 const double infinity = std::numeric_limits<double>::infinity();
 
-
-
-double hit(const vetor<double>& center, double radius, const raio<double>& raio) {
-    double a = produtoEscalar(raio.direcao, raio.direcao);
-    vetor<double> oc = subtracao(center, raio.origem);
-    double metadeb = produtoEscalar(raio.direcao, oc);
-    double b = 2.0 * produtoEscalar(raio.direcao, oc);
-    double c = produtoEscalar(oc, oc) - radius * radius;
-    double discriminant = metadeb * metadeb -  a * c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-metadeb - sqrt(discriminant)) /  a;
-    }
-}
-
+// define a cor do fundo
 vetor<double> backgroundColor(const vetor<double>& dir) {
     double t = 0.5 * (dir.y + 1.0);
     return vetor<double>((1 - t) * 1.0 + t * 0.5,
-                  (1 - t) * 1.0 + t * 0.7,
-                  (1 - t) * 1.0 + t * 1.0);
+                         (1 - t) * 1.0 + t * 0.7,
+                         (1 - t) * 1.0 + t * 1.0);
 }
-// define se um raio intersecta ou não um plano -> retorna true se intersecta, false caso contrário
-//  ax + by + cz + d = 0 -> equação do plano
-bool hitPlano(const vetor<double>& pontoNoPlano, const vetor<double>& normal, const raio<double>& raioDeInterseccao) {
-    double denominador = produtoEscalar(normal, raioDeInterseccao.direcao); // usado pra determinar se o raio é paralelo ao plano
-    // Verifica se o denominador é zero para evitar divisão por zero
-    if (fabs(denominador) < 1e-6) { // 0
-        return false; // Raio é paralelo ao plano -> não intersecta
-    }
-    // Calcula a distância para o ponto de interseção ao longo do raio
-    double t = -produtoEscalar(subtracao(raioDeInterseccao.origem, pontoNoPlano), normal) / denominador;
-    return t >= 0; // Se t for maior ou igual a zero, o raio intersecta o plano
-} //**************
 
-vetor<double> raioColor(const raio<double>& raio, malha mundo) {
+vetor<double> raioColor(const raio<double>& raio, malha mundo, sphere_list esferas) {
     hit_record rec;
-    vetor<double> pontoNoPlano(0, 0, 0); // Ajuste conforme necessário
-    vetor<double> normal(0, 1, 0);       // Ajuste conforme necessário
-
-    if(mundo.hit(pontoNoPlano, normal, raio, 0, infinity, rec)){
-        return multiplicacaoPorEscalar(vetor<double>(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1),0.5);
+    
+    plano plan(vetor<double>{0.0, -1.25, -1.0}, vetor<double>{0.0, 1.0, 2});
+    if(esferas.hit(raio, 0, infinity, rec)) {
+        return vetor<double>(1.0, 0.0, 0.0);
     }
+    else if(mundo.hit(raio, 0, infinity, rec)) {
+        return vetor<double>(0.0, 1.0, 0.0);
+    }
+    else if(plan.hitPlano(plan, raio)) {
+        return vetor<double>(0.2, 0.5, 0.9);
+    } 
+
     vetor<double> direcao_uni = vetorUni(raio.direcao);
-    auto t = 0.5 * (direcao_uni.y + 1.0);
-    return soma(multiplicacaoPorEscalar(vetor<double>(1.0, 1.0, 1.0), 1.0 - t), multiplicacaoPorEscalar(vetor<double>(0.5, 0.7, 1.0), t));
+    return backgroundColor(direcao_uni);
 }
 
 
-
- 
 int main() {
-    const int imWidth = 801;
+    // define a imagem
+    const int imWidth = 800;
     const int imHeight = static_cast<int>(imWidth / (16.0 / 9.0));
-    
     vector<vector<vetor<double>>> image(imHeight, vector<vetor<double>>(imWidth));
 
-    malha mundo;
-    // Adicione triângulos à malha
-    mundo.add(triangulo(vetor<double>{0, 0, -1}, vetor<double>{1, 0, -1}, vetor<double>{0, 1, -1}));
-    mundo.add(triangulo(vetor<double>{1, 0, -1}, vetor<double>{1, 1, -1}, vetor<double>{0, 1, -1}));
-    
-    vetor<double> origem{0.0, 0.0, 0.0};
-    double viewportHeight = 2.0;
-    double viewportWidth = viewportHeight * 16.0 / 9.0;
-    vetor<double> horizontal{viewportWidth, 0.0, 0.0};
-    vetor<double> vertical{0.0, viewportHeight, 0.0};
-    vetor<double> h2 = mult(0.5, horizontal);
-    vetor<double> h2o = subtracao(origem, h2);
-    vetor<double> v2 = mult(0.5, vertical);
-    vetor<double> lente{1, 0, -1};
-    vetor<double> v2f = subtracao(v2, lente);
-    vetor<double> lowerLeftCorner = subtracao(h2o, v2f);
+  
+    // define a câmera
+    vetor<double> posicaoDaCamera(0, 0, 0);
+    vetor<double> mira(0, 0, -1);
+    vetor<double> vUp(0, 1, 0);
+    Camera camera(posicaoDaCamera, mira, vUp);
 
+    // define o mundo
+    malha mundo;
+    sphere_list esferas;
+    
+    esferas.add(sphere(vetor<double>{0, 0, 2}, 0.5));
+    
+    // adiciona triângulos à malha
+    // malha de cima
+    double angulo60 = M_PI / 3; 
+    matriz4x4 rotacao = matriz4x4::createRotationZ(angulo60);
+    vetor<double> v1 = rotacao.multMatrizVetor(vetor<double>{1, 0, -1});
+    vetor<double> v2 = rotacao.multMatrizVetor(vetor<double>{1, -1, -1});
+    vetor<double> v3 = rotacao.multMatrizVetor(vetor<double>{2, 0, -1});
+    triangulo tri1(v1, v2, v3);
+    mundo.add(tri1);
+    mundo.add(triangulo(vetor<double>{0, 0, -1}, vetor<double>{0, -1, -1}, vetor<double>{1, 0, -1})); 
+    mundo.add(triangulo(vetor<double>{-1, 0, -1}, vetor<double>{-1, -1, -1}, vetor<double>{0, 0, -1}));
+    mundo.add(triangulo(vetor<double>{-2, 0, -1}, vetor<double>{-2, -1, -1}, vetor<double>{-1, 0, -1})); 
+
+    // parâmetros da classe triangulo: vetor<double> v0, vetor<double> v1, vetor<double> v2
+    // cada vetor<double> é um ponto no espaço 3D
+
+    // define a viewport
+    const vetor<double> larguraDaViewport(32.0 / 9.0, 0.0, 0.0);
+    const vetor<double> alturaDaViewport(0.0, 2.0, 0.0);
+    vetor<double> cantoEsquerdoTela = subtracao(subtracao(subtracao(camera.posicaoDaCamera, mult(0.5, larguraDaViewport)), mult(0.5, alturaDaViewport)), mira);
+    //vetor cantoEsquerdoTela = origem - horizontal/2 - vertical/2 - mira
+    
+    // define a cor do fundo
     for (int j = 0; j < imHeight; ++j) {
         for (int i = 0; i < imWidth; ++i) {
             double u = double(i) / (imWidth - 1);
             double v = 1.0 - double(j) / (imHeight - 1);
-            raio<double> r(origem, soma(lowerLeftCorner, soma(mult(u, horizontal), mult(v, vertical))));
-            vetor<double> color = raioColor(r, mundo);
-            image[j][i] = color; 
+            
+            vetor<double> direcaoDoRaio = subtracao(camera.posicaoDaCamera, soma(cantoEsquerdoTela, soma(mult(u, larguraDaViewport), mult(v, alturaDaViewport))));
+            raio<double> r(camera.posicaoDaCamera, direcaoDoRaio);
+            vetor<double> color = raioColor(r, mundo, esferas);
+            image[j][i] = color;
         }
     }
 
+    // output
     cout << "P3\n" << imWidth << " " << imHeight << "\n255\n";
     for (int j = 0; j < imHeight; ++j) {
         for (int i = 0; i < imWidth; ++i) {
@@ -110,7 +109,27 @@ int main() {
         }
     }
 
+    
+
+
     clog << "\rDone.                 \n";
+    
     return 0;
 }
 
+// Adicione triângulos à malha
+    // mundo.add(triangulo(vetor<double>{0, 0, -1}, vetor<double>{1, 0, -1}, vetor<double>{0, 1, -1}));
+    // mundo.add(triangulo(vetor<double>{1, 0, -1}, vetor<double>{1, 1, -1}, vetor<double>{0, 1, -1}));
+
+    // adiciona esferas ao mundo
+    // esferas.add(sphere(vetor<double>{0, 0, -1}, 0.5));
+    // esferas.add(sphere(vetor<double>{0, -1, -1}, 0.5));
+    // esferas.add(sphere(vetor<double>{1, 0, -1}, 0.5));
+
+
+ // mundo.add(triangulo(vetor<double>{-1, 0, -1}, vetor<double>{0, 0, -1}, vetor<double>{0, 1, -1})); // meio para baixo pra diretia
+    // mundo.add(triangulo(vetor<double>{0, 0, -1}, vetor<double>{0, 1, -1}, vetor<double>{1, 0, -1})); 
+
+    // mundo.add(triangulo(vetor<double>{0.8, 0, -1}, vetor<double>{0.8, -1, -1}, vetor<double>{1.8, 0, -1}));
+    // mundo.add(triangulo(vetor<double>{-1, 0, -1}, vetor<double>{-1, -1, -1}, vetor<double>{0, 0, -1}));
+    // mundo.add(triangulo(vetor<double>{-1, 0, -1}, vetor<double>{0, 1, -1}, vetor<double>{-1, 1, -1}));
