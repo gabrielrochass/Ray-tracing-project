d80df0bc46c5cae50966243d2b6bba36767b1f0d
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-08-16 12:09:40 -0300

    feat: explicação

--- textura.cpp
+++ textura.cpp
@@ -1,19 +1,23 @@
 #include "textura.h"
-#include <fstream>
-#include <stdexcept>
-#include <cmath>
+#include <iostream> 
+#include <fstream> // abre e lê arquivos
+#include <stdexcept> // lançamento de exceções
+#include <cmath> 
 
 Textura::Textura() : largura(0), altura(0) {}
 
+// carrega a textura de um arquivo
 Textura::Textura(const string& arquivo) {
-    ifstream arquivoTextura(arquivo, ios::binary);
+    ifstream arquivoTextura(arquivo, ios::binary); // abre o arquivo em modo binário
     if (arquivoTextura.is_open()) {
-        arquivoTextura.seekg(18, ios::beg);
-        arquivoTextura.read((char*)&largura, sizeof(largura));
+        arquivoTextura.seekg(18, ios::beg); // pula o cabeçalho do arquivo bmp de 18 bytes para chegar na largura e altura (imagem)
+        arquivoTextura.read((char*)&largura, sizeof(largura)); // lê a largura da imagem e armazena em largura
         arquivoTextura.read((char*)&altura, sizeof(altura));
-        arquivoTextura.seekg(54, ios::beg);
+        arquivoTextura.seekg(54, ios::beg); // pula o cabeçalho do arquivo bmp de 54 bytes para chegar nos dados da imagem (paleta de cores)
 
         textura.resize(altura);
+
+        // lê a paleta de cores da imagem e armazena em textura (matriz de cores)
         for (int i = 0; i < altura; i++) {
             textura[i].resize(largura);
             for (int j = 0; j < largura; j++) {
@@ -23,17 +27,20 @@ Textura::Textura(const string& arquivo) {
             }
         }
         arquivoTextura.close();
-    } else {
+    } else { // se não conseguir abrir o arquivo, lança uma exceção
         throw runtime_error("Erro ao abrir o arquivo de textura");
     }
 }
 
+// retorna a cor da textura em um ponto (u, v)
 vetor<double> Textura::corTextura(double u, double v) const {
+    // garante que u e v estão no intervalo [0, 1) calculando o resto da divisão por 1
     u = fmod(u, 1.0);
     v = fmod(v, 1.0);
 
+    // converte u e v para coordenadas da textura
     int i = (1 - v) * altura;
     int j = u * largura;
 
-    return textura[i][j];
+    return textura[i][j]; // retorna a cor da textura no ponto (u, v)
 }
--- textura.h
+++ textura.h
@@ -7,13 +7,14 @@
 
 using namespace std;
 
+// define a estrutura da textura
 struct Textura {
-    vector<vector<vetor<double>>> textura;
+    vector<vector<vetor<double>>> textura; // matriz de cores da textura
     int largura, altura;
 
     Textura();
-    Textura(const string& arquivo);
-    vetor<double> corTextura(double u, double v) const;
+    Textura(const string& arquivo); // carrega a textura de um arquivo
+    vetor<double> corTextura(double u, double v) const; // retorna a cor da textura em um ponto (u, v)
 };
 
 #endif
